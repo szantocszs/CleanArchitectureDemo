53f5d8a5d8b0fa5120d395c926415058c0684926
Szántó Csaba Zsolt <szantocszs@gmail.com>
2020-02-10 15:33:58 +0100

    Add new Api create function and swagger ui

--- CleanArch/CleanArch.Api/Controllers/CourseController.cs
+++ CleanArch/CleanArch.Api/Controllers/CourseController.cs
@@ -2,15 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace CleanArch.Api.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class CourseController : ControllerBase
     {
+        private readonly ICourseService _courseService;
 
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService ?? throw new ArgumentNullException(nameof(courseService));
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] CourseViewModel courseViewModel)
+        {
+            _courseService.Create(courseViewModel);
+
+            return Ok(courseViewModel);
+        }
     }
 }
