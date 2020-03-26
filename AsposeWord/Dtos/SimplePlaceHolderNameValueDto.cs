37f39d6c69fb6906fc50fbe0121d42b17f3b68df
SzantoCs <SzantoCs@HP-09385>
2020-03-26 11:48:00 +0100

    merge

--- AsposeWord/Dtos/SimplePlaceHolderNameValueDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Fortuna.Services.TemplateStorage.Interface.Dtos
-{
-    /// <summary>
-    /// Placeholder-szöveg pár adatait tároló osztály.
-    /// </summary>
-    public class SimplePlaceHolderNameValueDto : PlaceHolderNameValueDto<string>
-    {
-    }
-}
